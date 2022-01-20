db75a86056ab304f691d37959a1bd1e013ec090e
ymadd <Kb_yamato@protonmail.com>
2022-01-21 02:37:00 +0900

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const core = require('@actions/core');
+
+const firstGreeting = core.getInput('first-freeting');
+const secondGreeting = core.getInput('second-freeting');
+const thirdGreeting = core.getInput('third-freeting');
+
+console.log(`Hello ${firstFreeting}`);
+console.log(`Hello ${secondFreeting}`);
+if (thirdGreeting) {
+  console.log(`Hello ${thirdFreeting}`);
+}
