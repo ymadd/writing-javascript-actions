16b862fe98956599417c4afd79252eadee0f00fe
ymadd <Kb_yamato@protonmail.com>
2022-01-21 03:37:21 +0900

    create index.js

---
+++ .github/actions/issue-maker/index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const issueTitle = core.getInput('issue-title');
+    const jokeBody = core.getInput("joke")
+    const token = core.getinput("repo-taken")
+
+    const octokit = github.getOctokit(token)
+
+    await octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
