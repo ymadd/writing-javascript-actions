dcfccd7310dd5f9f8d7124182b8050bc770c1d27
ymadd <Kb_yamato@protonmail.com>
2022-01-21 02:38:41 +0900

    Fix code

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -4,8 +4,8 @@ const firstGreeting = core.getInput('first-freeting');
 const secondGreeting = core.getInput('second-freeting');
 const thirdGreeting = core.getInput('third-freeting');
 
-console.log(`Hello ${firstFreeting}`);
-console.log(`Hello ${secondFreeting}`);
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
 if (thirdGreeting) {
-  console.log(`Hello ${thirdFreeting}`);
+  console.log(`Hello ${thirdGreeting}`);
 }
